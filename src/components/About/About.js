244d8e6e986f5b3b64bfb06077737b6095144361
Amber Volkmann <volkmannamber@gmail.com>
2020-01-14 10:56:57 -0600

    file setup complete

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class About extends React.Component {
+
+    render() {
+        return (
+            
+            <footer>© React Actors</footer>
+            
+        )
+    }
+}
+
+export default About;
